d01a06b5dbfe05ab5853d55fe4d1db42851029c5
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-07-07 13:26:43 -0400

    psuedocode for routing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,27 @@
 const router = require('express').Router();
+
+///api/users//
+//get all users//
+
+//get single userby id//
+
+//post new user//
+
+//put to update a user by its id//
+
+//delete to remove user by its id//
+
+
+
+router
+    .route('/')
+    .get(getAllUsers)
+    
+
+
+
+///api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
