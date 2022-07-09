9cddcb30ae5dd8b30ca35c26ba3ca29fa95ffdf8
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-07-09 14:33:07 -0400

    added controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,107 @@
+//controller files contain functionality for the routes//
+const { User } = require('../models');
+
+
+const userController = {
+    //get all users//
+    getAllUser(req, res) {
+        User.find({}) //are brackets in brackets bc imported object//
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    //get single userby id//
+    getUserId({ params }, res) {
+        User.findOne({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    //post new user// why dont we need to assign username and email like we did before ? 
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    //put to update a user by its id//
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    //delete to remove user by its id//
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    // POST to add a new friend to a user's friend list
+
+    // DELETE to remove a friend from a user's friend list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+module.exports = userController;
+
+
